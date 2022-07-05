21fcf78b1bb747e033fc44392d581c2892f117d1
sarthak089 <106650128+sarthak089@users.noreply.github.com>
2022-07-05 15:09:36 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css'
 
 const about = () => {
   return (
+    <main>
     <div className={styles.content}>
           <h2>About</h2>
           <p>Sarthak Music started back in 2016 by Mr. Sarthak Sharma & within a period of 4 year, we ranked amongst the top 4 Music colleges in India. We Bagged Multiple Awards like  “Best Music Academy in India- 2018″
@@ -10,6 +11,12 @@ Excellence Teaching In Sound Engineering -2019
 Excellent Contribution in Music -2020
  Now with her sister Hritisha Rewadia, The opera queen of india, They extended it into a performing art college by the name of Angel’s Visuals & Performing Arts</p>
          </div>
+         
+         <footer className={styles.footer}>
+        <p><b>Powered: </b>SarthakTechsolutions</p>
+        </footer>
+         </main>
+         
   )
 }
 
